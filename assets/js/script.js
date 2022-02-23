444c2b1e266c055310868da203cf3a5742f47b70
Austin A <austinandrews89@gmail.com>
2022-02-22 17:07:16 -0700

    Work in progress. Main Card now populated with results from Fetch. Next step is building forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var geoCodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
 
 var searchUrl = "https://api.openweathermap.org/data/2.5/onecall?"
 
-var apiUrlAppend = "&appid=94690d5aa9031fa309833301cd16d30b"
+var apiUrlAppend = "&units=imperial&appid=94690d5aa9031fa309833301cd16d30b"
 var apiUrlAppend2 = ",us&appid=94690d5aa9031fa309833301cd16d30b"
 
 
@@ -62,6 +62,41 @@ function geoFetch(event){
                 console.log(response2);
                 console.log(response2.current.temp);
                 console.log(response2.daily[0].temp.day);
+
+                var mainEl = $("#mainCard");
+                var mainUlEl = $("<ul>");
+                var mainTempEl = $("<li class='temp'></li>");
+                var mainHumidEl = $("<li class='humidity'></li>");
+                var mainWindEl = $("<li class='wind'></li>");
+                var mainUvEl = $("<li class='uvi'></li>");
+                var headerEl = $("<h2>");
+
+                mainEl.empty();
+                mainEl.append(headerEl);
+                mainEl.children("h2").text(response[0].name+", "+response[0].state).css("display", "block");
+
+                mainEl.append(mainUlEl);
+
+                $("ul").append(mainTempEl);
+                $("ul").append(mainHumidEl);
+                $("ul").append(mainWindEl);
+                $("ul").append(mainUvEl);
+
+                $(".temp").text("Temperature: " + response2.current.temp + "F");
+                $(".humidity").text("Humidity: " + response2.current.humidity);
+                $(".wind").text("Wind Speed: " + response2.current.wind_speed + "mph");
+                $(".uvi").text("UV Index: " + response2.current.uvi);
+
+                console.log($(".temp").text())
+
+
+    
+
+
+
+
+
+
             })
 
             
